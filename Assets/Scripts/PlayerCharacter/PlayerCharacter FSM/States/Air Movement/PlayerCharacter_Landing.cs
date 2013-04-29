90498470201f9eeefa28669e92e87ee2f57ecfac
davidmfinol <davidmfinol@gmail.com>
2013-04-29 10:25:21 -0500

    Landing

--- Assets/Scripts/PlayerCharacter/PlayerCharacter FSM/States/Air Movement/PlayerCharacter_Landing.cs
+++ Assets/Scripts/PlayerCharacter/PlayerCharacter FSM/States/Air Movement/PlayerCharacter_Landing.cs
@@ -10,11 +10,13 @@ public class PlayerCharacter_Landing : PlayerCharacterStateMachineState
     {
         Controller.animation["Landing"].time = 0;
         if (!Controller.animation.IsPlaying("JumpLanding"))
-            Controller.animation.CrossFade("Landing");
-        if (Controller.FallHeight > 12)
         {
-            Controller.transform.GetComponentInChildren<HeartBox>().HitPoints -= (int)Controller.FallHeight * 20;
-            Controller.transform.GetComponentInChildren<PlayerHeartBox>().TimeSinceHit = 0;
+            Controller.animation.CrossFade("Landing");
+            if (Controller.FallHeight > 10)
+            {
+                Controller.transform.GetComponentInChildren<HeartBox>().HitPoints -= (int)Controller.FallHeight * 20;
+                Controller.transform.GetComponentInChildren<PlayerHeartBox>().TimeSinceHit = 0;
+            }
         }
     }
 
