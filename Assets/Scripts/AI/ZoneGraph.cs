e186c82a6ca5beef01f00ef6498411a586f12995
davidmfinol <davidmfinol@gmail.com>
2013-06-25 23:58:38 -0500

    Zombie brains...

---
+++ Assets/Scripts/AI/ZoneGraph.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+//Include generics to be able to use Generics
+using System.Collections.Generic;
+//Include the Pathfinding namespace to gain access to a lot of useful classes
+using Pathfinding;
+//using JsonFx.Json;
+//Inherit our new graph from a base graph type
+//[JsonOptIn]
+public class SimpleGraph : NavGraph
+{
+
+    public override void Scan()
+    {
+        //Here we will place our code for scanning the graph
+    }
+}
