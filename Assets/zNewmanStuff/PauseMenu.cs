7dde89a9ee10bec005396b7652e12edff46c4811
Newman Willis <newmanwillis@gmail.com>
2013-07-18 02:09:16 -0500

    I dont remember what I did

---
+++ Assets/zNewmanStuff/PauseMenu.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		KeyboardControl();
+	}
+	
+	void KeyboardControl(){
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			//Time.timeScale = 0;	
+			//transform.Find("
+			//gameObject.GetComponentInChildren(
+		}
+	}
+}
