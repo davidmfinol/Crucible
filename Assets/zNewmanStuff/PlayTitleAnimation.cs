4825a30870ba2cc7a427f4067ac3a0a4156da816
Newman Willis <newmanwillis@gmail.com>
2013-08-02 04:08:45 -0500

    Intro Title Animation

---
+++ Assets/zNewmanStuff/PlayTitleAnimation.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayTitleAnimation : MonoBehaviour {
+	
+	public int YPosTriggerAnimation = 40;
+	
+	private bool hasPlayedAnimation = false;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!hasPlayedAnimation && (GameLevel.Instance.Player.transform.position.y > YPosTriggerAnimation)){
+			transform.FindChild("TitleAnimPlane").transform.gameObject.SetActive(true);
+			hasPlayedAnimation = true;
+		}
+		//if(hasPlayedAnimation && transform.FindChild("TitleAnimPlane").gameObject){
+		//	gameObject.SetActive(false);	
+		//}
+	}
+	
+	/* //Needs to be rigidbody moving object
+	void OnTriggerEnter(Collider other){
+		if(other.Equals(GameLevel.Instance.Player)){
+			transform.FindChild("TitleAnimPlane").transform.gameObject.SetActive(true);				
+		}
+	}*/
+}
