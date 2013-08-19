bb5dab67e2058e755953984c59f4e6cd9702682b
Newman Willis <newmanwillis@gmail.com>
2013-08-19 07:57:17 -0500

    Runners in the Sewer

---
+++ Assets/zNewmanStuff/Pause/MenuFade.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class MenuFade : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//renderer.material.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	
+}
---
+++ Assets/zNewmanStuff/Pause/PauseMenu2.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class PauseMenu2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		KeyboardControl();
+	}
+	
+	void KeyboardControl(){
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			GameTime.Paused = !GameTime.Paused;
+			transform.FindChild("BG").transform.gameObject.SetActive(GameTime.Paused);
+			transform.FindChild("FG").transform.GetComponent<Fader>().FadeIn(Time.time);  //.GetComponent<"Fader">().FadeIn();
+			//transform.FindChild("FG").transform.gameObject.SetActive(GameTime.Paused);
+			//transform.FindChild("MG").transform.gameObject.SetActive(GameTime.Paused);
+
+			
+		}
+	}
+	
+	void OnGUI(){
+		//GUI.Box(new Rect(0, 0,Screen.width,Screen.height), "hello"); 	
+	}
+}
