2fa397378ccb4bb7ddf11684c45392ff891dd9d6
davidmfinol <davidmfinol@gmail.com>
2013-08-19 01:50:26 -0500

    Intro & Sewer

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -3,11 +3,17 @@ using System.Collections;
 
 public class Intro : MonoBehaviour
 {
-    public Transform Elevator = null;
+	public Transform TitlePrefab;
+    public Transform Elevator;
+    
 
 	void Start ()
 	{
-		Application.LoadLevelAdditive("Demo");
+		Application.LoadLevelAdditive("Sewer");
+		Transform titleAnim = (Transform) Instantiate(TitlePrefab, TitlePrefab.position, TitlePrefab.rotation);
+		titleAnim.parent = Camera.main.transform;
+		titleAnim.position = new Vector3(0f, 0f, 3.9f);
+		titleAnim.GetChild(0).position = Vector3.zero;
         GameLevel.Player.parent = Elevator;
         Camera.main.GetComponent<CameraScrolling>().Springiness = 1000;
         StartCoroutine("EndIntro");
