a22b060a4a349673ad68664880ea3511a3d2147f
thomascardwell7 <thomascardwell7@gmail.com>
2013-10-30 22:18:37 -0500

    NEW GUI

--- Assets/Scripts/Camera/CameraScript.cs
+++ Assets/Scripts/Camera/CameraScript.cs
@@ -11,7 +11,7 @@ public class CameraScript : MonoBehaviour
 	private Camera[] cameras;
 	private int currentCamIndex;
 	private Camera camCurrent;
-	private float threshold = 0.6f;
+	private float threshold = 0.5f;
 	public float sensitivity = 3f;
 	public float rotationSensitivity = 5f;
 	private float speed = 1;
@@ -61,12 +61,39 @@ public class CameraScript : MonoBehaviour
 				currentCamIndex++;
 		}
 		
+		if(Input.GetKey(KeyCode.Joystick1Button2))
+		{
+			Vector3 nextRot = new Vector3(camCurrent.transform.position.x, camCurrent.transform.position.y, camCurrent.transform.position.z);
+			camCurrent.transform.RotateAround(nextRot,new Vector3(0,0,1.0f),0.2f);
+		}
+		if(Input.GetKey(KeyCode.Joystick1Button1))
+		{
+			Vector3 nextRot = new Vector3(camCurrent.transform.position.x, camCurrent.transform.position.y, camCurrent.transform.position.z);
+			camCurrent.transform.RotateAround(nextRot,new Vector3(0,0,-1.0f),0.2f);
+		}
+		
+		if(Input.GetKey(KeyCode.Joystick1Button3))
+		{
+			if(camCurrent.transform.rotation.z == 360 || camCurrent.transform.rotation.z == -360)
+				camCurrent.transform.Rotate(new Vector3(camCurrent.transform.rotation.x, camCurrent.transform.rotation.y, 0));
+			Vector3 nextRot = new Vector3(camCurrent.transform.rotation.x, camCurrent.transform.rotation.y, camCurrent.transform.rotation.z-0.01f);
+			camCurrent.transform.Rotate(nextRot);
+		}
+		
 		SelectCamera(currentCamIndex);
 		
 		// For Position movement with the left stick
-		if((Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) && camCurrent != null && currentCamIndex != 0)
+		if((Input.GetAxis("Left Stick H") != 0 || Input.GetAxis("Left Stick V") != 0) && camCurrent != null && currentCamIndex != 0)
 		{
-			Vector3 nextPos = new Vector3(sensitivity*speed*0.01f*Input.GetAxis("Horizontal"),sensitivity*speed*0.01f*Input.GetAxis("Vertical"),0f);
+			if(Mathf.Abs(Input.GetAxis("Left Stick H")) < threshold)
+				horiz = 0.0f;
+			else horiz = Input.GetAxis("Left Stick H");
+			
+			if(Mathf.Abs(Input.GetAxis("Left Stick V")) < threshold)
+				vert = 0.0f;
+			else vert = Input.GetAxis("Left Stick V");
+			
+			Vector3 nextPos = new Vector3(sensitivity*speed*0.01f*Input.GetAxis("Left Stick H"),sensitivity*speed*0.01f*Input.GetAxis("Left Stick V"),0f);
 			camCurrent.transform.Translate(nextPos);
 		}
 		
