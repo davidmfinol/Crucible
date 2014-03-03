603caa0e0269ff4915bb103ebdbaf2dd909af4b4
davidmfinol <davidmfinol@gmail.com>
2014-03-02 18:29:57 -0600

    Some small fixes.

--- Assets/Scripts/Character/Player Character/PlayerCharacterAnimator.cs
+++ Assets/Scripts/Character/Player Character/PlayerCharacterAnimator.cs
@@ -110,9 +110,7 @@ public class PlayerCharacterAnimator : CharacterAnimator
 	protected override List<int> DefineRootMotionCorrectionState() // TODO: ERADICATE THIS METHOD
 	{
 		List<int> states = new List<int> ();
-//		states.Add (Animator.StringToHash ("Wall.Walljumping"));
 		states.Add (Animator.StringToHash ("Wall.Wallgrabbing"));
-//		states.Add (Animator.StringToHash ("Air.Falling"));
 		states.Add (Animator.StringToHash ("Base Layer.Damaged"));
 		states.Add (Animator.StringToHash ("Ground.StealthKill"));
 		return states;
@@ -496,7 +494,6 @@ public class PlayerCharacterAnimator : CharacterAnimator
 		else 
 			MecanimAnimator.SetBool(_hangHash, false);
 	}
-	
 
 	protected void Wallgrabbing(float elapsedTime)
 	{
@@ -645,7 +642,6 @@ public class PlayerCharacterAnimator : CharacterAnimator
 			}
 		}
 	}
-
 	
 	protected void ClimbingVertical(float elapsedTime)
 	{
