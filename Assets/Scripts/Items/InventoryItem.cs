27c8678957ed01c36faa8c2685407dea2e543a9a
Justin Hust <justin.hust@utexas.edu>
2014-03-07 14:42:33 -0600

    Materials not updated.

--- Assets/Scripts/Items/InventoryItem.cs
+++ Assets/Scripts/Items/InventoryItem.cs
@@ -6,7 +6,9 @@ public class InventoryItem {
 	public ItemType Type;
 	public int Quantity;
 	public int MaxQuantity;
-
+	public string Name;
+	public string Caption;
+	
 	public Texture GetTexture() {
 		// TODO: make actual inheritance hierarchy.
 		return null;
