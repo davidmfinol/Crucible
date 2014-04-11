702c1069e591f5a372ab1cc0bec307a420ee279e
thomascardwell7 <thomascardwell7@gmail.com>
2014-04-11 13:53:23 -0500

    Scripted elements in

--- Assets/Scripts/Story Events/Sewer/MysteriousRunner.cs
+++ Assets/Scripts/Story Events/Sewer/MysteriousRunner.cs
@@ -7,6 +7,9 @@ using System.Collections;
 [AddComponentMenu("Story Events/Sewer/Mysterious Runner")]
 public class MysteriousRunner : MonoBehaviour
 {
+	private bool _thrownBarrier = false;
+	private bool _walkedUnderneath = false;
+
     public IEnumerator ShowWallJump1 ()
     {
         CharacterInput input = GetComponent<CharacterInput> ();
@@ -16,14 +19,16 @@ public class MysteriousRunner : MonoBehaviour
         GameManager.IsPlayingCutscene = true;
 
         input.Horizontal = -1;
-        yield return new WaitForSeconds (1.05f);
+        yield return new WaitForSeconds (1.15f);
         input.Horizontal = 0;
         input.Jump = new Vector2 (-1, 1);
+		GameManager.MainCamera.Target = null;
         yield return new WaitForSeconds (0.2f);
         input.Jump = Vector2.zero;
         yield return new WaitForSeconds (0.7f);
         input.Jump = new Vector2 (1, 1);
         yield return new WaitForSeconds (1.5f);
+		input.Jump = new Vector2 (1, 1);
 
         GameManager.IsPlayingCutscene = false;
         GameManager.MainCamera.Target = GameManager.Player.transform;
@@ -42,9 +47,15 @@ public class MysteriousRunner : MonoBehaviour
         GameManager.IsPlayingCutscene = true;
 
         transform.position = startPosition;
-        input.Horizontal = 0.5f;
-        yield return new WaitForSeconds (8.0f);
+        input.Horizontal = 1.0f;
+        yield return new WaitForSeconds (0.3f);
 
+		input.Horizontal = 0.5f;
+		yield return new WaitForSeconds (2.5f);
+		GameManager.MainCamera.Target = null; 
+		yield return new WaitForSeconds (3.5f);
+
+		GameManager.MainCamera.Target = transform;
         GameManager.IsPlayingCutscene = false;
         GameManager.MainCamera.Target = GameManager.Player.transform;
         Destroy (gameObject.GetComponent<AudioListener> ());
@@ -52,4 +63,87 @@ public class MysteriousRunner : MonoBehaviour
         gameObject.SetActive (false);
 
     }
+
+	public IEnumerator ShowSightPuzzle (Vector3 startPosition)
+	{
+		CharacterInput input = GetComponent<CharacterInput> ();
+        Destroy (GameManager.Player.GetComponent<AudioListener> ());
+        this.gameObject.AddComponent<AudioListener> ();
+        GameManager.MainCamera.Target = transform;
+        GameManager.IsPlayingCutscene = true;
+
+        transform.position = startPosition;
+		input.Horizontal = -1f;
+		yield return new WaitForSeconds (1.2f);
+		input.Horizontal = 0f;
+		input.Vertical = 1.0f;
+		yield return new WaitForSeconds (2.5f);
+		input.Vertical = -1.0f;
+        yield return new WaitForSeconds (0.3f);
+		input.Vertical = 0.0f;
+		input.Horizontal = -0.5f;
+		yield return new WaitForSeconds (2.5f);
+		GameManager.MainCamera.Target = null;
+		input.Jump = new Vector2(-1, 1);
+		yield return new WaitForSeconds (0.2f);
+		input.Jump = new Vector2(-1, 1);
+		yield return new WaitForSeconds (3.5f);
+
+		GameManager.MainCamera.Target = transform;
+        GameManager.IsPlayingCutscene = false;
+        GameManager.MainCamera.Target = GameManager.Player.transform;
+        Destroy (gameObject.GetComponent<AudioListener> ());
+        GameManager.Player.gameObject.AddComponent<AudioListener> ();
+        gameObject.SetActive (false);
+	}
+
+	public IEnumerator ShowOlympusSpawn (Vector3 startPosition)
+	{
+		CharacterInput input = GetComponent<CharacterInput> ();
+		Destroy (GameManager.Player.GetComponent<AudioListener> ());
+		this.gameObject.AddComponent<AudioListener> ();
+		GameManager.MainCamera.Target = transform;
+		GameManager.IsPlayingCutscene = true;
+		
+		transform.position = startPosition;
+		input.Horizontal = -0.5f;
+		yield return new WaitForSeconds (1.5f);
+		_walkedUnderneath = true;
+		input.Horizontal = 0f;
+		yield return new WaitForSeconds (0.75f);
+		GameManager.MainCamera.Target = transform;
+		yield return new WaitForSeconds (0.5f);
+		input.Horizontal = 1f;
+		yield return new WaitForSeconds (0.1f);
+		input.Horizontal = 0f;
+		input.Attack = 1f;
+		_thrownBarrier = true;
+		yield return new WaitForSeconds (0.5f);
+		input.Attack = 0f;
+		GameManager.MainCamera.Target = null;
+		input.Horizontal = -1f;
+		yield return new WaitForSeconds (1.3f);
+		input.Jump = new Vector2(-1,1);
+		yield return new WaitForSeconds (3.5f);
+
+
+		GameManager.IsPlayingCutscene = false;
+		GameManager.UI.EnableInput ();
+		GameManager.MainCamera.Target = GameManager.Player.transform;
+		Destroy (gameObject.GetComponent<AudioListener> ());
+		GameManager.Player.gameObject.AddComponent<AudioListener> ();
+		gameObject.SetActive (false);
+	}
+
+	public bool WalkedUnderneath {
+		get {
+			return _walkedUnderneath;
+		}
+	}
+
+	public bool ThrownBarrier {
+		get {
+			return _thrownBarrier;
+		}
+	}
 }
--- Assets/Scripts/Story Events/Sewer/Tutorial.cs
+++ Assets/Scripts/Story Events/Sewer/Tutorial.cs
@@ -17,7 +17,11 @@ public class Tutorial : MonoBehaviour
 
     // The locations where certain effects will happen
     public Transform SneakStartPosition;
-    //public Transform Olympus1Position;
+	public Transform SightPuzzlePosition;
+	public Transform RunnerPositionOlympusSpawn;
+    public Transform Olympus1Position;
+	public Transform OlympusPrefab;
+	public Transform AIBarrier;
     //public Transform Olympus2Position;
 
     private bool _sewerDoorOpen;
@@ -94,6 +98,30 @@ public class Tutorial : MonoBehaviour
 
     }
 
+	public void ShowSightPuzzle ()
+	{
+		if (!GameManager.SaveData.HasShownSightPuzzle){
+			GameManager.SaveData.HasShownSightPuzzle = true;
+
+			Runner.gameObject.SetActive (true);
+			Runner.StartCoroutine (Runner.ShowSightPuzzle (SightPuzzlePosition.position));
+		}
+	}
+
+	public void ShowSpawnOlympus ()
+	{
+		if (!GameManager.SaveData.HasShownOlympusSpawn){
+			GameManager.SaveData.HasShownOlympusSpawn = true;
+			
+			Runner.gameObject.SetActive (true);
+			Runner.StartCoroutine (Runner.ShowOlympusSpawn (RunnerPositionOlympusSpawn.position));
+
+			StartCoroutine(SpawnOlympus());
+			GameManager.IsPlayingCutscene = true;
+			GameManager.UI.DisableInput ();
+		}
+	}
+
     public void BeforeCamera ()
     {
         SewerDoor.animation.Play ("Open");
@@ -121,5 +149,32 @@ public class Tutorial : MonoBehaviour
         }
         
     }
+
+	public IEnumerator SpawnOlympus ()
+	{
+		while(Runner.WalkedUnderneath == false)
+			yield return null;
+		GameManager.MainCamera.Target = Olympus1Position.transform;
+		yield return new WaitForSeconds (0.5f);
+		
+		// spawn olympus up high and have him fall.
+		Transform newOlympus = (Transform)Instantiate (OlympusPrefab, Olympus1Position.position, Quaternion.identity);
+		GameManager.MainCamera.Target = newOlympus;
+		newOlympus.GetComponent<EnemyAISettings> ().ShouldWander = false;
+		newOlympus.GetComponent<CharacterAnimator> ().Direction = new Vector3 (-1.0f, 0.0f, 0.0f);
+		
+		// drops 2 items.
+		newOlympus.GetComponent<ItemDropper> ().AddItem (Item.ItemType.Item_ComputerParts);
+
+		StartCoroutine(ShowBarrier());
+	
+	}
+
+	public IEnumerator ShowBarrier ()
+	{
+		while(Runner.ThrownBarrier == false)
+			yield return null;
+		AIBarrier.gameObject.SetActive(true);
+	}
 
 }
