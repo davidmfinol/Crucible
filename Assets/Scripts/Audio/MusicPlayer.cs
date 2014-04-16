328a253ee28bfbf2bdd9cc9a7b1a9a3ea6b6cbc5
edace91 <ed.ace91@gmail.com>
2014-04-16 17:54:18 -0500

    More Music updates

---
+++ Assets/Scripts/Audio/MusicPlayer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Audio/Music Player")]
+public class MusicPlayer : AudioPlayer
+{
+	public AudioClip[] Music;
+	public float PercentCompleteToNext;
+	public float MaxTimeBetweenWinds;
+	public float MaxPanSpread;
+	private AudioSource[] _audios;
+	private int _currentAudio;
+	
+	void Start ()
+	{
+		_audios = GetComponents<AudioSource> ();
+		foreach (AudioSource audiosource in _audios) {
+			audiosource.volume = 0.09f;
+		}
+		_currentAudio = 0;
+		StartCoroutine (DelayedMusicPlay ());
+		
+	}
+	
+	public IEnumerator DelayedMusicPlay ()
+	{
+		while (true) {
+			// First, decide when to play the wind
+			float timeToPlay = Random.Range (0.0f, MaxTimeBetweenWinds);
+			yield return new WaitForSeconds (timeToPlay);
+			
+			// Then play a random wind sound
+			int windex = Random.Range (0, Music.Length);
+			float pan = Random.Range (-MaxPanSpread, MaxPanSpread);
+			_audios [_currentAudio].clip = Music [windex];
+			_audios [_currentAudio].pan = pan;
+			_audios [_currentAudio].Play ();
+			
+			// Then wait until this wind sound is nearing completion
+			float normalizedTime = ((float)_audios [_currentAudio].timeSamples) / ((float)_audios [_currentAudio].clip.samples);
+			while (normalizedTime < PercentCompleteToNext) {
+				normalizedTime = ((float)_audios [_currentAudio].timeSamples) / ((float)_audios [_currentAudio].clip.samples);
+				yield return null;
+			}
+			
+			_currentAudio++;
+			_currentAudio = _currentAudio % 2;
+		}
+		
+	}
+	
+}
--- Assets/Scripts/Character/Non-Player Character/Olympus/OlympusAnimator.cs
+++ Assets/Scripts/Character/Non-Player Character/Olympus/OlympusAnimator.cs
@@ -156,7 +156,7 @@ public class OlympusAnimator : CharacterAnimator
 
     protected void Punch (float elapsedTime)
     {
-		_sound.Play (_sound.Idling, 0.5f);
+		_sound.Play (_sound.Idling, 0.3f);
         // find where to place the attack event
         Vector3 meleePos = transform.position;
         meleePos.x += (2.5f * Direction.x);
@@ -523,7 +523,7 @@ public class OlympusAnimator : CharacterAnimator
 
 	public void PlayServo()
 	{
-		_sound.Play (_sound.Idling, 0.5f);
+		_sound.Play (_sound.Idling, 0.3f);
 	}
 
     public override bool IsLanding {
